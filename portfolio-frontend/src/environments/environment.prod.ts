9274ae2a190bdb102ce9f77556e9f3bc9877466a
FRANCO EXEQUIEL SCHULER ALLUB <franschuler13@gmail.com>
2022-10-22 18:14:22 +0200

    changed production enviroment

--- portfolio-frontend/src/environments/environment.prod.ts
+++ portfolio-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  url: 'https://portfolio-mockbackend.herokuapp.com'
+  url: 'https://portfolio-mockbackend.herokuapp.com',
+  email: 'franschuler13@gmail.com',
+  password: 'portfolio_FS10'
 };
