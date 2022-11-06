63ea1ecbc4c433c553d158cf5b67b7f9e1b51954
FRANCO EXEQUIEL SCHULER ALLUB <franschuler13@gmail.com>
2022-11-06 21:42:42 +0100

    messages section

--- portfolio-frontend/src/app/components/contactme/contactme.component.ts
+++ portfolio-frontend/src/app/components/contactme/contactme.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Message } from 'src/app/models/Message';
+import { AuthService } from 'src/app/services/auth.service';
 import { MessageService } from 'src/app/services/message.service';
 import { ConfirmationComponent } from '../confirmation/confirmation.component';
 
@@ -13,19 +15,31 @@ import { ConfirmationComponent } from '../confirmation/confirmation.component';
 export class ContactmeComponent implements OnInit {
 
   contactForm: any;
+  messages: Message[] = [];
+  isLogged: boolean = false;
 
   constructor(private fb: FormBuilder,
+              private authService: AuthService,
               private messageService: MessageService,
               private dialog: MatDialog,
               private snackBar: MatSnackBar
               ) { }
 
   ngOnInit(): void {
+    this.isLogged = this.authService.isLoggedIn();
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       message: ['', Validators.required]
-    })
+    });
+    this.getMessages();
+  }
+
+  getMessages() {
+    this.messageService.getMessages()
+      .subscribe((res:any) => {
+        this.messages = res;
+      })
   }
 
   sendMessage(){
